4a60963c3ca407d3db0c40b49a45e6b3c961fcbb
uvsq22108780 <91671391+uvsq22108780@users.noreply.github.com>
2021-10-04 01:26:40 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 public class Main {
-    public static void main (String [] args) {
 
 
-    }
+        public static void main(String[] args) {
+            FractionSimple r = new FractionSimple();
+
+            System.out.println("entrer la valeur du numérateur \n");
+            Scanner scanner = new Scanner(System.in);
+            Float num = scanner.nextFloat();
+            r.seta(num);
+
+            System.out.println("entrer la valeur du dénominateur \n");
+            float denum = scanner.nextFloat();
+            assert r.getb() !=0;
+            r.affichage();
+            r.setb(denum);
+
+
+        }
+
 }
