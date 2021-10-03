9bcda997ae0c74b4314d2961041b16699ee4c4c1
uvsq22108780 <91671391+uvsq22108780@users.noreply.github.com>
2021-10-04 01:25:53 +0200

    Update FractionSimple.java

--- src/FractionSimple.java
+++ src/FractionSimple.java
@@ -1,4 +1,31 @@
+
 public class FractionSimple {
+
+    float a;
+    float b;
+
+        public float geta () {
+            return a ; }
+
+        public void seta (  float a) {
+            this.a=a;}
+
+        public float getb () {
+            return b ; }
+
+         public void setb ( float a) {
+            if( b!=0){
+           this.b=b;}
+            else { System.out.println("le denominateur ne peut pas etre 0 ");}
+        }
+
+    public String affichage() {
+
+        System.out.println("votre fraction est \n "+a+"/"+b);
+        return null ;         }
+
+
+
 
 
 
